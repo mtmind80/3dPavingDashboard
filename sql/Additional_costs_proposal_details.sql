7245696f44f1d2958a2be864ed2788bf8463df56
Michael Trachtenberg <mtrachtenberg@fsx.com>
2023-07-01 08:24:06 -0400

    WIP

--- sql/Additional_costs_proposal_details.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `proposal_detail_additional_costs` VALUES (2049, 32937, 20011, 23.00, 'Other', 'test', '2023-06-07 18:23:00', '2023-06-07 18:23:00');
-INSERT INTO `proposal_detail_additional_costs` VALUES (2052, 32937, 20011, 3243.00, 'Dump Fee', 'test', '2023-06-15 12:18:07', '2023-06-15 12:18:07');
-INSERT INTO `proposal_detail_additional_costs` VALUES (2050, 32940, 20011, 343.00, 'Other', 'test', '2023-06-13 14:42:44', '2023-06-13 14:42:44');
-INSERT INTO `proposal_detail_additional_costs` VALUES (2051, 32947, 20011, 344.00, 'Dump Fee', 'test', '2023-06-14 14:46:12', '2023-06-14 14:46:12');
