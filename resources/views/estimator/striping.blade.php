7b224475488d1715a4ea7bba8d1ceeaf5a7b950f
Michael Trachtenberg <MTrachtenberg@fsx.com>
2023-09-17 15:45:19 -0400

    WIP

--- resources/views/estimator/striping.blade.php
+++ resources/views/estimator/striping.blade.php
@@ -15,19 +15,7 @@
             @lang('translation.estimator') Striping Pavement Markings
         @endslot
     @endcomponent
-<script>
 
-    // Create our number formatter.
-    const formatCurrency = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-        // These options are needed to round to whole numbers if that's what you want.
-        //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-        //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
-    });
-
-
-</script>
     <div class="row">
 
         <form method="POST" action="{{route('save_striping')}}" id="estimator_form" class="custom-validation admin-form w-100">
