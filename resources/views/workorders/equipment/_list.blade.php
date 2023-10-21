6f565895b79e1bb0e158c21add80481ec21f52bb
Jose Vidal <jose.vidal@traderinteractive.com>
2023-10-20 21:57:17 -0400

    Modified field report view for workorder service.

---
+++ resources/views/workorders/equipment/_list.blade.php
@@ -0,0 +1,23 @@
+@if (!empty($equipments) && $equipments->count() > 0)
+    @foreach ($equipments as $equipment)
+        <tr id="equipment_{{ $equipment->id }}">
+            <td class="tc">{{ $equipment->report_date->format('m/d/Y') }}</td>
+            <td class="tc">{{ $equipment->name }}</td>
+            <td class="tc">{{ $equipment->hours }}</td>
+            <td class="tc">{{ $equipment->rate_type }}</td>
+            <td class="tc">{{ $equipment->html_rate }}</td>
+            <td class="tc">{{ $equipment->number_of_units }}</td>
+            <td class="centered">
+                <button
+                    class="btn p0 btn-danger tc delete-equipment-button"
+                    type="button"
+                    data-toggle="tooltip"
+                    title="Delete item"
+                    data-equipment_id="{{ $equipment->id }}"
+                >
+                    <i class="far fa-trash-alt dib m0 plr5"></i>
+                </button>
+            </td>
+        </tr>
+    @endforeach
+@endif
