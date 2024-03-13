339e9ef8e855c3888ccb3d68efccd882be608e65
Michael Trachtenberg <MTrachtenberg@fsx.com>
2024-03-13 15:04:53 -0400

    WIP

--- database/migrations/2022_02_25_170140_create_permits_table.php
+++ database/migrations/2022_02_25_170140_create_permits_table.php
@@ -19,10 +19,10 @@ class CreatePermitsTable extends Migration
             $table->unsignedBigInteger('proposal_id')->index();
             $table->unsignedBigInteger('proposal_detail_id')->default(null)->nullable();
             $table->enum('status', ['Approved','Completed', 'Not Submitted', 'Submitted', 'Under Review','Comments'])->default('Not Submitted');
-            $table->enum('type', ['Building', 'Engineering', 'Miscellaneous'])->default('Regular')->nullable();
+            $table->enum('type', ['Building', 'Engineering', 'Miscellaneous'])->default('Building')->nullable();
             $table->string('number', 25)->default(null)->nullable();
             $table->string('county', 100)->default(null)->nullable();
-            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('created_by')->nullable();
             $table->unsignedBigInteger('last_updated_by');
             $table->timestamps();
 
