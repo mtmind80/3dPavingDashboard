d1ca51dfee1fa22e4ac60d0ac31a5c24fe07e060
Michael Trachtenberg <mike.trachtenberg@gmail.com>
2024-12-23 11:12:19 -0500

    WIP

--- resources/views/proposals/_proposal_services.blade.php
+++ resources/views/proposals/_proposal_services.blade.php
@@ -2,9 +2,9 @@
     <thead>
         <tr>
             <th class="tc fwb">@lang('translation.proposalservices')</th>
-            <th class="tc fwb">@lang('translation.status')</th>
+            <!--  <th class="tc fwb">@lang('translation.status')</th>
             <th class="tc fwb">@lang('translation.location')</th>
-            <!-- <th class="tc fwb">@lang('translation.fieldmanager')</th>  -->
+           <th class="tc fwb">@lang('translation.fieldmanager')</th>  -->
             <th class="tc fwb">@lang('translation.cost')</th>
             <th class="tc fwb">@lang('translation.order')</th>
             <th class="actions tc">@lang('translation.actions')</th>
@@ -14,9 +14,9 @@
         @foreach ($services as $service)
             <tr data-id="{{ $service->id }}">
                 <td class="tc"><a href="{{ route('edit_service', ['proposal_id' => $service->proposal_id, 'id' => $service->id]) }}">{{ $service->service_name }}</a></td>
-                <td class="tc">{{ $service->status->status ?? 'No Status' }}</td>
+                <!--<td class="tc">{{ $service->status->status ?? 'No Status' }}</td>
                 <td class="tc">{!! $service->location->full_location_two_lines ?? 'No Location Specified' !!}</td>
-                <!-- <td class="tc">{{ $service->fieldmanager->full_name ?? 'No Manager Assigned' }}</td> -->
+                <td class="tc">{{ $service->fieldmanager->full_name ?? 'No Manager Assigned' }}</td> -->
                 <td class="tc">{{ \App\Helpers\Currency::format($service->cost ?? '0.0') }}</td>
                 <td class="tc">{{ $service->html_dsort }}</td>
                 <td class="centered actions">
